a5b32823ba9f8447a25145b0cf630f76b184992a
johnclary <john.clary@austintexas.gov>
2016-01-07 15:33:59 -0600

    fix marker images (!)

--- js/net-map.js
+++ js/net-map.js
@@ -144,8 +144,6 @@ function getEvents(){
 
 function makeMap(){
 	
-	L.Icon.Default.imagePath = "./images"; //leaflet didn't like the network path it was creating during testing, see (https://github.com/Leaflet/Leaflet/issues/766)
-	
 	var OSMBase = new L.TileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 			attribution: '<a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> | <a href="https://www.austintexas.gov/department/code" target=_blank>City of Austin Code Department</a>'
 		});
@@ -281,77 +279,3 @@ $(".dropdown-menu li a").click(function(){
     $(this).closest('div').find('button[data-toggle="dropdown"]').html(selText + ' <span class="caret"></span>');
 });
 
-
-
-////debug
-(function (window) {
-
-  // Stores past URLs that failed to load. Used for a quick lookup
-  // and because `onerror` is not triggered in some browsers
-  // if the response is cached.
-  var errors = {};
-
-  // Check the existence of an image file at `url` by creating a
-  // temporary Image element. The `success` callback is called
-  // if the image loads correctly or the image is already complete.
-  // The `failure` callback is called if the image fails to load
-  // or has failed to load in the past.
-  window.checkImage = function (url, success, failure) {
-    var img = new Image(),    // the 
-        loaded = false,
-        errored = false;
-
-    // Run only once, when `loaded` is false. If `success` is a
-    // function, it is called with `img` as the context.
-    img.onload = function () {
-      if (loaded) {
-        return;
-      }
-
-      loaded = true;
-
-      if (success && success.call) {
-        success.call(img);
-      }
-    };
-
-    // Run only once, when `errored` is false. If `failure` is a
-    // function, it is called with `img` as the context.
-    img.onerror = function () {
-      if (errored) {
-        return;
-      }
-
-      errors[url] = errored = true;
-
-      if (failure && failure.call) {
-        failure.call(img);
-      }
-    };
-
-    // If `url` is in the `errors` object, trigger the `onerror`
-    // callback.
-    if (errors[url]) {
-      img.onerror.call(img);
-      return;
-    }
-    
-    // Set the img src to trigger loading
-    img.src = url;
-
-    // If the image is already complete (i.e. cached), trigger the
-    // `onload` callback.
-    if (img.complete) {
-      img.onload.call(img);
-    }
-  };
-
-})(this);
-
-function success() {
-console.log("success: ", this.src);
-}
-
-function failure() {
-console.log("failure: ", this.src);
-}
